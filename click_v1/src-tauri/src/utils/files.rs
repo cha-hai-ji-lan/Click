687b51eaf826a9af13444d9ef3e5961d57ff3d31
ChaHaiJiLan <shi2760992374@outlook.com>
2025-12-16 01:38:32 +0800

    0.1.2.3-2025-12-16rust修改文件夹解析时间排序的返回枚举类型

--- click_v1/src-tauri/src/utils/files.rs
+++ click_v1/src-tauri/src/utils/files.rs
@@ -1,25 +1,34 @@
 use std::fs; // 用于文件操作
 use std::io; // 用于错误处理
-use std::path::Path; // 用于路径操作
+use std::time; // 用于获取文件创建时间
 
+#[derive(Debug)]
 pub struct FolderObject {
     pub name: String,
     pub path: String,
-    pub created_time: String,
+    pub created_time: time::SystemTime,
     pub father_name: String,
 }
+///
+/// ## `time::SystemTime`: 表示一个系统时间
+/// > 实现了`debug trait`，因此可以在打印时使用
+#[derive(Debug)]
 pub struct FileObject {
     pub name: String,
     pub path: String,
     pub size: u64,
-    pub created_time: String,
+    pub created_time: time::SystemTime,
     pub extension_name: String,
-    pub father_folder_name: String,
     pub father_name: String,
 }
 
 impl FolderObject {
-    pub fn new(name: String, path: String, created_time: String, father_name: String) -> Self {
+    pub fn new(
+        name: String,
+        path: String,
+        created_time: time::SystemTime,
+        father_name: String,
+    ) -> Self {
         FolderObject {
             name,
             path,
@@ -34,9 +43,8 @@ impl FileObject {
         name: String,
         path: String,
         size: u64,
-        created_time: String,
+        created_time: time::SystemTime,
         extension_name: String,
-        father_folder_name: String,
         father_name: String,
     ) -> Self {
         FileObject {
@@ -45,7 +53,6 @@ impl FileObject {
             size,
             created_time,
             extension_name,
-            father_folder_name,
             father_name,
         }
     }
@@ -56,57 +63,145 @@ impl FileObject {
 
 #[derive(Debug)]
 pub enum FileSystemObject {
-    File(Vec<(String, String, u64, std::time::SystemTime)>),
-    Folder(Vec<(String, String, std::time::SystemTime)>),
-    Combined(
-        Vec<(String, String, u64, std::time::SystemTime)>,
-        Vec<(String, String, std::time::SystemTime)>,
-    ),
+    File(Vec<FileObject>),
+    Folder(Vec<FolderObject>),
+    Combined(Vec<FileObject>, Vec<FolderObject>),
 }
 
 fn list_files_by_time(
     dir_path: &str,
     mod_option: Option<i32>,
 ) -> Result<FileSystemObject, io::Error> {
-    let mut files_with_time: Vec<(String, String, u64, std::time::SystemTime)> = Vec::new();
-    let mut folder_with_time: Vec<(String, String, std::time::SystemTime)> = Vec::new();
-
-    // 读取目录条目
-    for entry in fs::read_dir(dir_path)? {
-        let entry = entry?;
-        let path = entry.path(); // 获取文件路径
-                                 // 只处理文件（可选：也可以包含目录）
-        if path.is_file() {
-            let file_name = path.file_name().unwrap().to_string_lossy().to_string(); // 获取文件名
-            let file_path = path.to_string_lossy().to_string(); // 获取文件路径
-            let metadata = fs::metadata(&path)?; // 获取文件元数据 文件大小、文件类型、创建时间、
-            let file_size = metadata.len(); // 获取修改时间
-            let file_modified_time = metadata.modified()?; // 获取修改时间
-
-            files_with_time.push((file_name, file_path, file_size, file_modified_time));
-        } else {
-            // 处理目录
-            let dir_path = path.to_string_lossy().to_string(); // 获取目录路径
-            let dir_name = path.file_name().unwrap().to_string_lossy().to_string(); // 获取目录名
-            let metadata = fs::metadata(&path)?; // 获取目录元数据
-            let dir_modified_time = metadata.modified()?; // 获取修改时间
-            folder_with_time.push((dir_name, dir_path, dir_modified_time));
-        }
-    }
-
-    // 按修改时间排序（从旧到新）
-    files_with_time.sort_by_key(|(_, _, _, time)| *time);
-    folder_with_time.sort_by_key(|(_, _, time)| *time);
+    let mut files_with_time: Vec<FileObject> = Vec::new();
+    let mut folder_with_time: Vec<FolderObject> = Vec::new();
     let param = mod_option.unwrap_or(1);
     match param {
         1 => {
+            // 解析文件条目
+            for entry in fs::read_dir(dir_path)? {
+                let entry = entry?;
+                let path = entry.path(); // 获取文件路径
+                // 只处理文件（可选：也可以包含目录）
+                if path.is_file() {
+                    let file_name = path.file_stem().unwrap().to_string_lossy().to_string(); // 获取文件名
+                    let file_path = path.to_string_lossy().to_string(); // 获取文件路径
+                    let metadata = fs::metadata(&path)?; // 获取文件元数据 文件大小、文件类型、创建时间、
+                    let file_size = metadata.len(); // 获取修改时间
+                    let file_modified_time = metadata.modified()?; // 获取修改时间
+                    let file_extension_name =
+                        path.extension().unwrap().to_string_lossy().to_string();
+                    let father_dir_name = path
+                        .parent()
+                        .unwrap()
+                        .file_name()
+                        .unwrap()
+                        .to_string_lossy()
+                        .to_string();
+
+                    files_with_time.push(FileObject::new(
+                        file_name,
+                        file_path,
+                        file_size,
+                        file_modified_time,
+                        file_extension_name,
+                        father_dir_name,
+                    ));
+                }
+            }
+            // 按修改时间排序（从旧到新）
+            files_with_time.sort_by_key(|file| file.created_time);
             Ok(FileSystemObject::File(files_with_time))
         }
         2 => {
+            // 解析目录条目
+            for entry in fs::read_dir(dir_path)? {
+                let entry = entry?;
+                let path = entry.path(); // 获取文件路径
+                // 只处理文件（可选：也可以包含目录）
+                if !path.is_file() {
+                    let dir_path = path.to_string_lossy().to_string(); // 获取目录路径
+                    let dir_name = path.file_name().unwrap().to_string_lossy().to_string(); // 获取目录名
+                    let metadata = fs::metadata(&path)?; // 获取目录元数据
+                    let dir_modified_time = metadata.modified()?; // 获取修改时间
+                    let father_dir_name = path
+                        .parent()
+                        .unwrap()
+                        .file_name()
+                        .unwrap()
+                        .to_string_lossy()
+                        .to_string();
+
+                    folder_with_time.push(FolderObject::new(
+                        dir_name,
+                        dir_path,
+                        dir_modified_time,
+                        father_dir_name,
+                    ));
+                }
+            }
+
+            // 按修改时间排序（从旧到新）
+            folder_with_time.sort_by_key(|folder| folder.created_time);
             Ok(FileSystemObject::Folder(folder_with_time))
         }
         _ => {
-            Ok(FileSystemObject::Combined(files_with_time, folder_with_time))
+            // 解析文件和目录条目
+            for entry in fs::read_dir(dir_path)? {
+                let entry = entry?;
+                let path = entry.path(); // 获取文件路径
+                if path.is_file() {
+                    let file_name = path.file_stem().unwrap().to_string_lossy().to_string(); // 获取文件名
+                    let file_path = path.to_string_lossy().to_string(); // 获取文件路径
+                    let metadata = fs::metadata(&path)?; // 获取文件元数据 文件大小、文件类型、创建时间、
+                    let file_size = metadata.len(); // 获取修改时间
+                    let file_modified_time = metadata.modified()?; // 获取修改时间
+                    let file_extension_name =
+                        path.extension().unwrap().to_string_lossy().to_string();
+                    let father_dir_name = path
+                        .parent()
+                        .unwrap()
+                        .file_name()
+                        .unwrap()
+                        .to_string_lossy()
+                        .to_string();
+
+                    files_with_time.push(FileObject::new(
+                        file_name,
+                        file_path,
+                        file_size,
+                        file_modified_time,
+                        file_extension_name,
+                        father_dir_name, ));
+                } else {
+                    // 处理目录
+                    let dir_path = path.to_string_lossy().to_string(); // 获取目录路径
+                    let dir_name = path.file_name().unwrap().to_string_lossy().to_string(); // 获取目录名
+                    let metadata = fs::metadata(&path)?; // 获取目录元数据
+                    let dir_modified_time = metadata.modified()?; // 获取修改时间
+                    let father_dir_name = path
+                        .parent()
+                        .unwrap()
+                        .file_name()
+                        .unwrap()
+                        .to_string_lossy()
+                        .to_string();
+
+                    folder_with_time.push(FolderObject::new(
+                        dir_name,
+                        dir_path,
+                        dir_modified_time,
+                        father_dir_name,
+                    ));
+                }
+            }
+
+            // 按修改时间排序（从旧到新）
+            files_with_time.sort_by_key(|file| file.created_time);
+            folder_with_time.sort_by_key(|folder| folder.created_time);
+            Ok(FileSystemObject::Combined(
+                files_with_time,
+                folder_with_time,
+            ))
         }
     }
 }
